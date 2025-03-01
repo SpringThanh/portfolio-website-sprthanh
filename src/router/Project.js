d460c30e63d00cec1f75fc6dec5941b5463215c5
SpringThanh <nastuthanh2@gmail.com>
2025-03-01 23:34:06 +0700

    feat: portfolio footer

--- src/router/Project.js
+++ src/router/Project.js
@@ -1,6 +1,14 @@
 //./router/Project.js
 import React from "react";
 
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
 export const Project = () => {
-  return <div>Project</div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 };
